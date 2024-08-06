fb0c858bbd972b8c390325440b3522ea80c86220
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-08-05 23:12:26 -0400

    refactoring

--- src/components/NewChapterDreidels.tsx
+++ src/components/NewChapterDreidels.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Box, Button, Container } from '@mui/material';
 import { Dreidel, Side } from '../components/Dreidel';
 
 export const NewChapterDreidels = () => {
     const [chapterStartDreidels, setChapterStartDreidels] = useState<(Side | undefined)[]>([]);
 
-    function poissonGenerator(lambda: number) {    
+    const poissonGenerator = (lambda: number) => {    
         let usedElapsed = 0;
         let numberOfEvents = 0;
         while (usedElapsed < 1) {
@@ -14,18 +14,22 @@ export const NewChapterDreidels = () => {
             numberOfEvents += 1;
         }
         return numberOfEvents;
+    };
+
+    const setStartDreidelIndex = (index:number) => (side: Side | undefined) => {
+        chapterStartDreidels[index] = side;
+        setChapterStartDreidels([...chapterStartDreidels]);
+    }
+
+    const spinStartDreidels = () => {
+        let numberOfDreidels = poissonGenerator(3);
+        setChapterStartDreidels(Array.from(Array(numberOfDreidels)));
     }
 
     return <Container>
         <Box>Chapter Start Spin</Box>
-        {chapterStartDreidels.map((side, index) => <Dreidel side={side} endTime={20} onLand={(side) => {
-            chapterStartDreidels[index] = side;
-            setChapterStartDreidels([...chapterStartDreidels]);
-        }}/>)}
-        {chapterStartDreidels.some(side => side === undefined) || <Button onClick={() => {
-            let numberOfDreidels = poissonGenerator(3);
-            setChapterStartDreidels(Array.from(Array(numberOfDreidels)));
-        }}>Spin New Chapter Dreidels</Button>}
+        {chapterStartDreidels.map((side, index) => <Dreidel side={side} endTime={5*chapterStartDreidels.length - 2 * index} onLand={setStartDreidelIndex(index)}/>)}
+        {chapterStartDreidels.some(side => side === undefined) || <Button onClick={spinStartDreidels}>Spin New Chapter Dreidels</Button>}
 
     </Container>;
 }
