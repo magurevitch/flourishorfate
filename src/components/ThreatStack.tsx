72269e4af2381a74775c60e044208f37f713d2cc
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-08-18 22:06:19 -0400

    added threat stack

---
+++ src/components/ThreatStack.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Stack, TextField } from '@mui/material';
+import CircleIcon from '@mui/icons-material/Circle';
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+
+export const ThreatStack = () => {
+    const MAX_STACK = 12;
+
+    const [stackSize, setStackSize] = useState<number>(0);
+    const [ephemeral, setEphemeral] = useState<number>(0);
+    const [gelt, setGelt] = useState<boolean[]>([]);
+
+    useEffect(() => {
+        let filled = ephemeral > 0 ? Math.min(stackSize, ephemeral) : stackSize;
+        let outlined = Math.max(stackSize, ephemeral) - filled;
+        let stack = new Array(filled).fill(true)
+        stack = stack.concat(new Array(outlined).fill(false));
+        setGelt(stack);
+    }, [stackSize, ephemeral]);
+
+    const positionInElement = (yPosition: number, target: EventTarget & HTMLDivElement) => {
+        let fromTop = MAX_STACK * (yPosition - target.offsetTop) / target.offsetHeight;
+        return MAX_STACK - Math.floor(fromTop);
+    }
+
+    return <Grid container spacing={2}>
+        <Grid item>
+            <Stack
+                direction='column-reverse'
+                onMouseMove={(e) => setEphemeral(positionInElement(e.pageY, e.currentTarget))}
+                onMouseLeave={() => setEphemeral(0)}
+                onClick={(e) => setStackSize(positionInElement(e.pageY, e.currentTarget))}
+                sx={{height: 24 * MAX_STACK, width: 24, bgcolor: 'warning.light'}}
+            >
+                {gelt.map(isReal => isReal ? <CircleIcon/> : <CircleOutlinedIcon />)}
+            </Stack>
+        </Grid>
+        <Grid item>
+            {ephemeral || stackSize}
+            <TextField type='number' onChange={(e) => {
+                let value = parseInt(e.target.value);
+                if(0 <= value && value <= MAX_STACK)
+                    setStackSize(value);
+            }}/>
+            {stackSize === 0 || <Button onClick={() => setStackSize(stackSize - 1)}>Remove Gelt</Button>}
+        </Grid>
+    </Grid>;
+}
--- src/pages/DreidelSpinner.tsx
+++ src/pages/DreidelSpinner.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Box, Button, Container } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { ActionDreidels } from '../components/ActionDreidels';
 import { NewChapterDreidels } from '../components/NewChapterDreidels';
+import { ThreatStack } from '../components/ThreatStack';
 
 export const DreidelSpinner = () => {
-    return <Container>
-        <Button><Link to="/dybbuks-and-dreidels">Back to Main</Link></Button>
-        <Box>
+    return <Grid container>
+        <Grid item>
+            <Button><Link to="/dybbuks-and-dreidels">Back to Main</Link></Button>
+        </Grid>
+        <Grid item>
             Dybbuks and Dreidels has 4 different types of dreidel spin. The Action Spin and the Evil Eye spin can use the action dreidel, and the Support Spin and the Chapter Start Spin have their own buttons. 
-        </Box>
-        <ActionDreidels />
-        <NewChapterDreidels />
-    </Container>;
+        </Grid>
+        <Grid item>
+            <ActionDreidels />
+        </Grid>
+        <Grid item>
+            <NewChapterDreidels />
+        </Grid>
+        <Grid item>
+            <ThreatStack />
+        </Grid>
+    </Grid>;
 }
