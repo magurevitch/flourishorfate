f521af1183d8383060808103b8132829e1ab130f
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-08-20 00:03:16 -0400

    adding in images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Box, Button, Container } from '@mui/material';
 import { Email } from '@mui/icons-material';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export const Footer = () => {
     return <Box component="footer" position="fixed" bottom="0px" alignSelf="flex-end" padding="0px" sx={{
         bgcolor: 'primary.main',
         width: '100%'
     }}>
-        <Container sx={{color: 'white'}}>
-            Flourish or Fate Games
-            <Button><a href='mailto:flourishorfate@gmail.com'><Email />  Email</a></Button>
+        <Container sx={{color: 'white',display: 'flex',alignItems: 'center',flexWrap: 'wrap'}}>
+            <div>Flourish or Fate Games</div>
+            <Button sx={{color: 'white'}} onClick={() => {window.location.href = 'mailto:flourishorfate@gmail.com';}} startIcon={<Email />}>Email</Button>
+            <div style={{display: 'flex',alignItems: 'center',flexWrap: 'wrap',}}><CopyrightIcon /> 2024 Matthew Gurevitch</div>
         </Container>
     </Box>
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import { Button, Icon } from '@mui/material';
+import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import {default as FlourishOrFateLogo} from '../images/FlourishOrFateIcon.png';
+import {default as DybbuksAndDreidelsCover} from '../images/DybbuksAndDreidelsCover.png';
 
 export const Navbar = () => {
     return <AppBar position='fixed'>
         <Toolbar>
-            <Button><Link to="/"><img src="/FlourishOrFateIcon.svg" alt="flourish or fate logo" height="25" width="25" />Home</Link></Button>
-            <Button><Link to="/dybbuks-and-dreidels">Dybbuks and Dreidels</Link></Button>
+            <Button startIcon={<img src={FlourishOrFateLogo} alt="flourish or fate logo" height="25" width="25" />}><Link to="/">Home</Link></Button>
+            <Button startIcon={<img src={DybbuksAndDreidelsCover} alt="dybbuks and dreidels cover" height="25" width="25"/>}><Link to="/dybbuks-and-dreidels">Dybbuks and Dreidels</Link></Button>
         </Toolbar>
     </AppBar>;
 }
