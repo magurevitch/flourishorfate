58a584025455e4b7fefb66b3e14a0f9facaf9886
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-08-27 18:15:51 -0400

    making Dybbuks and Dreidels splash page look nice

--- src/pages/DybbuksAndDreidels.tsx
+++ src/pages/DybbuksAndDreidels.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import { FAQ } from '../components/FAQ';
 import {default as DybbuksAndDreidelsCover} from '../images/DybbuksAndDreidelsCoverText.jpg';
 
 
 export const DybbuksAndDreidels = () => {
-    return <Container>
-        <Typography>Dybbuks and Dreidels</Typography>
-        Dybbuks and Dreidels is a Table-Top Role Playing Game inspired by Jewish history and culture.
-        The rulebook will soon be available on <a href="https://www.drivethrurpg.com/">DriveThruRPG</a>
-        <a href='https://www.drivethrurpg.com/'><img src={DybbuksAndDreidelsCover} alt="Dybbuks and Dreidels Cover" width="50%"/></a>
-        <Button><Link to="/dybbuks-and-dreidels/dreidel-spinner">Online Dreidel Spinner</Link></Button>
-        <FAQ />
-    </Container>;
+    return <Grid container>
+        <Grid item sm={12}><Typography>Dybbuks and Dreidels</Typography></Grid>
+        <Grid item sm={9}>
+            Dybbuks and Dreidels is a Table-Top Role Playing Game inspired by Jewish history and culture.
+            The rulebook will soon be available on <a href="https://www.drivethrurpg.com/">DriveThruRPG</a>.
+            The game is intended for one-shot storytelling, for 3-7 people.
+        </Grid>
+        <Grid item sm={3}>
+            <a href='https://www.drivethrurpg.com/'><img src={DybbuksAndDreidelsCover} alt="Dybbuks and Dreidels Cover" width="100%"/></a>
+        </Grid>
+        <Grid item sm={12}>
+            <Link to="/dybbuks-and-dreidels/dreidel-spinner"><Button sx={{
+                color: 'white',
+                bgcolor: 'primary.main'
+            }}>Online Dreidel Spinner</Button></Link>
+        </Grid>
+        <Grid item sm={12}><FAQ /></Grid>
+    </Grid>;
 }
