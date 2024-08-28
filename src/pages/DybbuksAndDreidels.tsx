6bc4d90d0222af623b386f004b6f9f471bf3964c
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-08-28 19:13:19 -0400

    moving a link

--- src/pages/DybbuksAndDreidels.tsx
+++ src/pages/DybbuksAndDreidels.tsx
@@ -12,15 +12,15 @@ export const DybbuksAndDreidels = () => {
             Dybbuks and Dreidels is a Table-Top Role Playing Game inspired by Jewish history and culture.
             The rulebook will soon be available on <a href="https://www.drivethrurpg.com/">DriveThruRPG</a>.
             The game is intended for one-shot storytelling, for 3-7 people.
-        </Grid>
-        <Grid item sm={3}>
-            <a href='https://www.drivethrurpg.com/'><img src={DybbuksAndDreidelsCover} alt="Dybbuks and Dreidels Cover" width="100%"/></a>
-        </Grid>
-        <Grid item sm={12}>
+
+            <br/>
             <Link to="/dybbuks-and-dreidels/dreidel-spinner"><Button sx={{
                 color: 'white',
                 bgcolor: 'primary.main'
             }}>Online Dreidel Spinner</Button></Link>
+        </Grid>
+        <Grid item sm={3}>
+            <a href='https://www.drivethrurpg.com/'><img src={DybbuksAndDreidelsCover} alt="Dybbuks and Dreidels Cover" width="100%"/></a>
         </Grid>
         <Grid item sm={12}><FAQ /></Grid>
     </Grid>;
