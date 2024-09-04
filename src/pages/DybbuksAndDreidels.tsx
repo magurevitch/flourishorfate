342ba4191b49ed183c134b3d61de71c2f353c4d3
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-09-04 18:26:11 -0400

    now the game is live!

--- src/pages/DybbuksAndDreidels.tsx
+++ src/pages/DybbuksAndDreidels.tsx
@@ -10,8 +10,9 @@ export const DybbuksAndDreidels = () => {
         <Grid item sm={9}>
             <Typography variant='h4'>Dybbuks and Dreidels</Typography>
             Dybbuks and Dreidels is a Table-Top Role Playing Game inspired by Jewish history and culture.
-            The rulebook will soon be available on <a href="https://www.drivethrurpg.com/">DriveThruRPG</a>.
-            The game is intended for one-shot storytelling, for 3-7 people.
+            The rulebook is available on <a href="https://www.drivethrurpg.com/en/product/493884/dybbuks-and-dreidels">DriveThruRPG</a>.
+            It is currently only available in PDF, but I am working on getting the book out soon as well.
+            The game is intended for one-shot storytelling, for 4-7 people.
 
             <br/>
             <Link to="/dybbuks-and-dreidels/dreidel-spinner"><Button sx={{
@@ -20,7 +21,7 @@ export const DybbuksAndDreidels = () => {
             }}>Online Dreidel Spinner</Button></Link>
         </Grid>
         <Grid item sm={3}>
-            <a href='https://www.drivethrurpg.com/'><img src={DybbuksAndDreidelsCover} alt="Dybbuks and Dreidels Cover" width="100%"/></a>
+            <a href='https://www.drivethrurpg.com/en/product/493884/dybbuks-and-dreidels'><img src={DybbuksAndDreidelsCover} alt="Dybbuks and Dreidels Cover" width="100%"/></a>
         </Grid>
         <Grid item sm={12}><FAQ /></Grid>
     </Grid>;
