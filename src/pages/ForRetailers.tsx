28c5e2169311a3e1200f1c66f5eaafadfac1b175
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-10-22 18:17:42 -0400

    making it nicely <p>

--- src/pages/ForRetailers.tsx
+++ src/pages/ForRetailers.tsx
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 
 export const ForRetailers = () => {
     return <div>
-        Hi retailers! Thank you for your interest in Flourish or Fate Games!
-
-        My games are published through <a href="https://www.drivethrurpg.com/en/publisher/28340/flourish-or-fate-games">DriveThruRPG</a>.
-        To request a code for the retailer discount, please e-mail me at <a href='mailto:flourishorfate@gmail.com'>FlourishOrFate@gmail.com</a>.
+        <p>
+            Hi retailers! Thank you for your interest in Flourish or Fate Games!
+        </p>
+        <p>
+            My games are published through <a href="https://www.drivethrurpg.com/en/publisher/28340/flourish-or-fate-games">DriveThruRPG</a>.
+            To request a code for the retailer discount, please e-mail me at <a href='mailto:flourishorfate@gmail.com'>FlourishOrFate@gmail.com</a>.
+        </p>
     </div>;
 }
