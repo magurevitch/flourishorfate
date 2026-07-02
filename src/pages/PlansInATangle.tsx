900811f17c862d87cfbe8c2e4ccb9dacf6baede4
Matthew Gurevitch <matthewgurevitch@gmail.com>
2026-07-02 15:51:59 -0400

    adding in more copy

--- src/pages/PlansInATangle.tsx
+++ src/pages/PlansInATangle.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import { EmailLink } from '../components/EmailLink';
+import { Grid } from '@mui/material';
 
 export const PlansInATangle: React.FC = () => {
-    return <div>
-        <i>Plans in a Tangle</i> is a GM-less game for 3-8 people, coming out in July 2026.
-        <b>Plans in a Tangle</b> is a work in progress, expected to come out Summer of 2026.
-        It is a GM-less game, with 3 simultaneous heists happening in the same museum.
-        Every player is on all 3 teams at once, with mechanics allowing the interactions, more chaos, and bringing back already established plot points.
-        Please e-mail me at <EmailLink /> if you would like an advance copy.
-    </div>;
+    return <Grid container>
+        <Grid item>
+            <i>Plans in a Tangle</i> is a GM-less game, coming out in July 2026.
+        </Grid>
+
+        <Grid item bgcolor="warning.light" sx={{border: 2, borderColor: 'text.primary', padding: "10px", borderRadius: "8px"}}>
+            Dawn breaks through a broken skylight.
+            A getaway car burned tire marks from the West Wing of the Museum.
+            Police are scratching their heads.
+            Wasn’t the painting in the East Wing?
+            The alarm went off in the New Annex,
+            and the neighbors heard a helicopter.
+            It wasn’t one heist stealing away the artifacts;
+            there must have been three last night.
+        </Grid>
+       
+        <Grid item>
+            In <i>Plans in a Tangle</i>,
+            3-8 players weave together the stories of three heist teams striking the same museum at the same time.
+            All players are involved in all three stories.
+        </Grid>
+
+        <Grid>
+            Players: 3-8
+            <br/>
+            Time:
+            <br/>
+            2 ½ hours if someone read therules thoroughly
+            <br/>
+            3 ½ hours if you skimmed and are trying to figure it out on the fly
+        </Grid>
+
+        <Grid>
+            Please e-mail me at <EmailLink /> if you would like an advance copy.
+        </Grid>
+    </Grid>;
 }
