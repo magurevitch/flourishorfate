b58f3ed4930c99dca97b6e51cb9c59c51dffade5
Matthew Gurevitch <matthewgurevitch@gmail.com>
2026-07-02 18:16:07 -0400

    formatting and pictures

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,14 @@ import Toolbar from '@mui/material/Toolbar';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import {default as FlourishOrFateLogo} from '../images/FlourishOrFateIcon.png';
+import {default as PlansInATangleLogo} from '../images/PlansInATangle/Tangle.png';
 import {default as DybbuksAndDreidelsCover} from '../images/DybbuksAndDreidels/Cover.png';
 
 export const Navbar: React.FC = () => {
     return <AppBar position='fixed'>
         <Toolbar>
             <Link to="/"><Button sx={{color:"white"}} startIcon={<img src={FlourishOrFateLogo} alt="flourish or fate logo" height="40" />}>Home</Button></Link>
-            <Link to="/plans-in-a-tangle"><Button sx={{color:"white"}}>Plans in a Tangle (Upcoming)</Button></Link>
+            <Link to="/plans-in-a-tangle"><Button sx={{color:"white"}} startIcon={<img src={PlansInATangleLogo} alt="plans in a tangle logo" height="40"/>}>Plans in a Tangle</Button></Link>
             <Link to="/dybbuks-and-dreidels"><Button sx={{color:"white"}} startIcon={<img src={DybbuksAndDreidelsCover} alt="dybbuks and dreidels cover" height="40"/>}>Dybbuks and Dreidels</Button></Link>
             <Link to="/for-retailers"><Button sx={{color:"white"}}>For Retailers</Button></Link>
             <Link to="/upcoming"><Button sx={{color:"white"}}>Upcoming Games</Button></Link>
--- src/pages/PlansInATangle.tsx
+++ src/pages/PlansInATangle.tsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import { EmailLink } from '../components/EmailLink';
-import { Grid } from '@mui/material';
+import { Box, Grid, Stack } from '@mui/material';
+import {default as CatBurgler} from '../images/PlansInATangle/LadyInBlack.png';
+import {default as SanLuisObispo} from '../images/PlansInATangle/LadyInYellow.png';
+
 
 export const PlansInATangle: React.FC = () => {
     return <Grid container>
-        <Grid item>
-            <i>Plans in a Tangle</i> is a GM-less game, coming out in July 2026.
+        <Grid item sm={8}>
+            <Stack>
+                <Box>
+                    <i>Plans in a Tangle</i> is a GM-less game, coming out in July 2026.
+                </Box>
+                <Box bgcolor="warning.light" sx={{border: 2, borderColor: 'text.primary', padding: "10px", borderRadius: "8px"}}>
+                    Dawn breaks through a broken skylight.
+                    A getaway car burned tire marks from the West Wing of the Museum.
+                    Police are scratching their heads.
+                    Wasn’t the painting in the East Wing?
+                    The alarm went off in the New Annex,
+                    and the neighbors heard a helicopter.
+                    It wasn’t one heist stealing away the artifacts;
+                    there must have been three last night.
+                </Box>
+                <Box>
+                    In <i>Plans in a Tangle</i>,
+                    3-8 players weave together the stories of three heist teams striking the same museum at the same time.
+                    All players are involved in all three stories.
+                </Box>
+            </Stack>
         </Grid>
 
-        <Grid item bgcolor="warning.light" sx={{border: 2, borderColor: 'text.primary', padding: "10px", borderRadius: "8px"}}>
-            Dawn breaks through a broken skylight.
-            A getaway car burned tire marks from the West Wing of the Museum.
-            Police are scratching their heads.
-            Wasn’t the painting in the East Wing?
-            The alarm went off in the New Annex,
-            and the neighbors heard a helicopter.
-            It wasn’t one heist stealing away the artifacts;
-            there must have been three last night.
-        </Grid>
-       
-        <Grid item>
-            In <i>Plans in a Tangle</i>,
-            3-8 players weave together the stories of three heist teams striking the same museum at the same time.
-            All players are involved in all three stories.
+        <Grid item sm={4}>
+            <img src={CatBurgler} alt="Cat Burgler" width="100%"/>
         </Grid>
 
-        <Grid>
-            Players: 3-8
-            <br/>
-            Time:
-            <br/>
-            2 ½ hours if someone read therules thoroughly
-            <br/>
-            3 ½ hours if you skimmed and are trying to figure it out on the fly
+        <Grid item sm={4}>
+            <img src={SanLuisObispo} alt="Lady in Yellow" width="100%"/>
         </Grid>
 
-        <Grid>
+        <Grid item sm={8}>
+            <Stack>
+                <Box>Players: 3-8</Box>
+                <Box>Time:</Box>
+                <Box>2 ½ hours if someone read therules thoroughly</Box>
+                <Box>3 ½ hours if you skimmed and are trying to figure it out on the fly</Box>
+            </Stack>
+        </Grid>
+
+        <Grid item sm={12}>
             Please e-mail me at <EmailLink /> if you would like an advance copy.
         </Grid>
     </Grid>;
